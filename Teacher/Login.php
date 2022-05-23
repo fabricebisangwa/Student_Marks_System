9591249008d262085875695909cda88b097660b2
UMUTUZO Marie Aime <99897862+umutuzo@users.noreply.github.com>
2022-05-23 12:20:57 -0700

    update

--- Teacher/Login.php
+++ Teacher/Login.php
@@ -8,7 +8,7 @@ $upass = $_POST['password'];
 if(isset($_POST['submit'])) 
 { 
  
-  $query ="SELECT * FROM teacher WHERE Username=:uname AND password=:upass";
+  $query ="SELECT * FRM teacher WHERE Username=:uname AND password=:upass";
   $stmt = $connection->prepare($query);
   $stmt->execute([':uname' => $uname,':upass' => $upass]);
   $countRow = $stmt->rowCount();
